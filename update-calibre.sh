32c12d3f1b349b6857be88c7a871a6b42a98a955
Alejandro Anzola <anzola95avila@gmail.com>
2017-09-02 11:27:20 -0500

    agrega script para actualizar directorio de dropbox con la biblioteca de calibre

---
+++ update-calibre.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+notify-send -u critical "Doing Rsync of Calibre Library"
+rsync -avz --delete "/mnt/homedsk/skinman95/Calibre Library" "/mnt/homedsk/skinman95/Dropbox" > "/home/skinman95/rsync$(date '+%R::%F').log" || notify-send -u critical "Error sincronizando Calibre Library a Dropbox"
