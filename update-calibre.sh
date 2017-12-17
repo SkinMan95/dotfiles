910b30aa805d556af9b5032ee3bedcf710e1bf06
Alejandro Anzola <anzola95avila@gmail.com>
2017-12-16 20:34:49 -0500

    corrige script de calibre para mostrar correctamente mensajes en pantalla

--- update-calibre.sh
+++ update-calibre.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+DISPLAY=:0.0
 notify-send -u critical "Doing Rsync of Calibre Library"
 rsync -avz --delete "/mnt/homedsk/skinman95/Calibre Library" "/mnt/homedsk/skinman95/Dropbox" | tee "/home/skinman95/calibre_logs_bkp/rsync$(date '+%R::%F').log" || notify-send -u critical "Error sincronizando Calibre Library a Dropbox"
