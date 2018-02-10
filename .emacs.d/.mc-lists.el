6f9daa7776d6d56a0aed00cf5395782ed7a219c7
Alejandro Anzola <anzola95avila@gmail.com>
2018-02-09 20:30:48 -0500

    en emacs agrega modo de movimiento de lineas o regiones de texto completas y modo de generacion de markdown (por problema en la generacion de utf-8)

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -3,6 +3,7 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+	markdown-outdent-or-delete
 	sgml-slash
 	))
 
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -162,3 +162,83 @@
 (change-theme 'magonyx 'weyland-yutani)
 
 (setq inhibit-startup-screen t)
+
+;; markdown with utf-8
+(eval-after-load "markdown-mode"
+  '(defalias 'markdown-add-xhtml-header-and-footer 'as/markdown-add-xhtml-header-and-footer))
+
+(defun as/markdown-add-xhtml-header-and-footer (title)
+    "Wrap XHTML header and footer with given TITLE around current buffer."
+    (goto-char (point-min))
+    (insert "<!DOCTYPE html5>\n"
+	    "<html>\n"
+	    "<head>\n<title>")
+    (insert title)
+    (insert "</title>\n")
+    (insert "<meta charset=\"utf-8\" />\n")
+    (when (> (length markdown-css-paths) 0)
+      (insert (mapconcat 'markdown-stylesheet-link-string markdown-css-paths "\n")))
+    (insert "\n</head>\n\n"
+	    "<body>\n\n")
+    (goto-char (point-max))
+    (insert "\n"
+	    "</body>\n"
+	    "</html>\n"))
+
+;; move lines and regions
+;; lines
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+;; region
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+;; lines and region
+(defun move-line-region-up (&optional start end n)
+  (interactive "r\np")
+  (if (use-region-p) (move-region-up start end n) (move-line-up n)))
+
+(defun move-line-region-down (&optional start end n)
+  (interactive "r\np")
+  (if (use-region-p) (move-region-down start end n) (move-line-down n)))
+
+(global-set-key (kbd "M-p") 'move-line-region-up)
+(global-set-key (kbd "M-n") 'move-line-region-down)
